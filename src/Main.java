b8f7d39279e4bf26968f2446b8a861fa8aace4be
chailatt <chevezrobertson@yahoo.com>
2022-09-26 14:13:13 -0400

    jk

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Enter Your Birth Number!");
+        int bNum = 13;
+
+        if (bNum >= 1 && bNum <= 12)
+        {
+            System.out.println("Your birth number is " + bNum);
+        }
+        else
+        System.out.println("You entered an incorrect number");
     }
 }
